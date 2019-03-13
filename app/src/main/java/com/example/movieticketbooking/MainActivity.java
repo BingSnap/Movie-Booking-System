39b8b8fae1d3948bedc3ab7bc9d15aa2dbc5b012
Kyle Bingham <33461342+BingSnap@users.noreply.github.com>
2019-03-12 21:40:59 -0700

    Project Initialization
    Added first few screens for my movie booking phone application.

---
+++ app/src/main/java/com/example/movieticketbooking/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.movieticketbooking;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void Wonder(View v){
+        Intent myIntent = new Intent(getBaseContext(), WonderPark.class);
+        startActivity(myIntent);
+    }
+
+    public void Feet(View v){
+        Intent myIntent = new Intent(getBaseContext(), FiveFeetApart.class);
+        startActivity(myIntent);
+    }
+
+
+    public void Marvel(View v){
+        Intent myIntent = new Intent(getBaseContext(), CaptainMarvel.class);
+        startActivity(myIntent);
+    }
+
+
+    public void Dragon(View v){
+        Intent myIntent = new Intent(getBaseContext(), HowToTrainYourDragon.class);
+        startActivity(myIntent);
+        }
+
+
+
+}
---
+++ app/src/main/java/com/example/movieticketbooking/WonderPark.java
@@ -0,0 +1,13 @@
+package com.example.movieticketbooking;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class WonderPark extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.wonderpark);
+    }
+}

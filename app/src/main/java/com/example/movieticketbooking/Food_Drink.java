82bdffb91ded693f6d3c2cabd59729fd8681d8c4
Kyle Bingham <33461342+BingSnap@users.noreply.github.com>
2019-03-22 14:44:05 -0700

    Multiple Changes
    Design and functionality changes.

--- app/src/main/java/com/example/movieticketbooking/Food_Drink.java
+++ app/src/main/java/com/example/movieticketbooking/Food_Drink.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.Spinner;
 
 public class Food_Drink extends AppCompatActivity {
     TextView text;
@@ -14,24 +16,34 @@ public class Food_Drink extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.food_drinks);
-        //text = (TextView) findViewById(R.id.textView2);
+        /*text = (TextView) findViewById(R.id.textView2);
 
-        /*Bundle extras = getIntent().getExtras();
+        Bundle extras = getIntent().getExtras();
         if (extras != null) {
             String value = extras.getString("key");
             text.setText(value);
         }*/
+
+        //Various drop down menus for quantity and sizes
+        Spinner popcornSizeDropdown = findViewById(R.id.spinner1);
+        Spinner drinkSizeDropdown = findViewById(R.id.spinner2);
+        Spinner popcornQuantityDropdown = findViewById(R.id.spinner);
+        Spinner drinkQuantityDropdown = findViewById(R.id.spinner3);
+
+        String[] sizes = new String[]{"Small", "Medium", "Large"};
+        String[] quantity = new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+
+
+        ArrayAdapter<String> sizeAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, sizes);
+        ArrayAdapter<String> quantityAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, quantity);
+
+        popcornSizeDropdown.setAdapter(sizeAdapter);
+        drinkSizeDropdown.setAdapter(sizeAdapter);
+        popcornQuantityDropdown.setAdapter(quantityAdapter);
+        drinkQuantityDropdown.setAdapter(quantityAdapter);
     }
 
-    public void onClick(View v) {
-        data = getIntent();
-        String first = data.getExtras().getString("time");
-        if (data.hasExtra("time")) {
-            text.setText(first);
-        } else {
-            text.setText("CAT");
-        }
-    }
+
 
     /*@Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
--- app/src/main/java/com/example/movieticketbooking/WonderPark.java
+++ app/src/main/java/com/example/movieticketbooking/WonderPark.java
@@ -1,13 +1,47 @@
 package com.example.movieticketbooking;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class WonderPark extends AppCompatActivity {
 
+    Button button;
+    String buttonText;
+    Intent i;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.wonderpark);
+    }
+
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.time1:
+                button = (Button)findViewById(R.id.time1);
+                buttonText = button.getText().toString();
+                i = new Intent(WonderPark.this, Food_Drink.class);
+                i.putExtra("key",buttonText);
+                startActivity(i);
+                break;
+            case R.id.time2:
+                button = (Button)findViewById(R.id.time2);
+                buttonText = button.getText().toString();
+                Intent i = new Intent(WonderPark.this, Food_Drink.class);
+                i.putExtra("key",buttonText);
+                startActivity(i);
+                break;
+            case R.id.time3:
+                button = (Button)findViewById(R.id.time3);
+                buttonText = button.getText().toString();
+                i = new Intent(WonderPark.this, Food_Drink.class);
+                i.putExtra("key",buttonText);
+                startActivity(i);
+                break;
+            default:
+                break;
+        }
     }
 }
